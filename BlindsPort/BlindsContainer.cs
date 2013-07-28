d99be104b818b46d926fb805b028ac1a6b0d1858
Germán Valencia <machinarius92@gmail.com>
2013-07-28 00:24:55 -0500

    Started the migration to LinearLayout, it's acting slightly weird though... no widgets can be seen

--- BlindsPort/BlindsContainer.cs
+++ BlindsPort/BlindsContainer.cs
@@ -4,15 +4,27 @@ using Android.Content;
 using Android.Util;
 using Android.Graphics;
 using Android.Graphics.Drawables;
+using Android.Widget;
 
 namespace BlindsPort {
 	/// <summary>
 	/// This is a port of the BlindsView class built by Sony and explained at 
 	/// http://developer.sonymobile.com/knowledge-base/tutorials/ui-graphics/use-the-blindsview-effect-from-the-xperia-lockscreen/
 	/// 
-	/// All credits go to them for the ideas and Machinarius for the port.
+	///
+	/// Copyright (c) 2013, Sony Mobile Communications AB
+	/// All rights reserved.
+
+	///	Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+	/// * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+	/// * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+	/// * Neither the name of Sony Mobile nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+	///	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+	/// @author Johan Henricson
+	/// ported to Xamarin.Android by German "Machinarius" Valencia
 	/// </summary>
-	public class BlindsContainer : ViewGroup {
+	public class BlindsContainer : LinearLayout {
 
 		private static Color BackgroundColor {
 			get { return Color.Rgb (250, 50, 50); }
@@ -44,10 +56,6 @@ namespace BlindsPort {
 			ForegroundPaint = new Paint () {
 				Color = ForegroundColor
 			};
-		}
-
-		protected override void OnLayout (bool changed, int left, int top, int right, int bottom) {
-
 		}
 
 		protected override void DispatchDraw (Canvas canvas) {
--- BlindsPort/Resources/Resource.designer.cs
+++ BlindsPort/Resources/Resource.designer.cs
@@ -63,8 +63,23 @@ namespace BlindsPort
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050002
+			public const int MediumTextBox = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int SmallTextBox = 2131034115;
+			
 			// aapt resource value: 0x7f050000
 			public const int blindsContainer = 2131034112;
+			
+			// aapt resource value: 0x7f050004
+			public const int dummybutton1 = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int dummybutton2 = 2131034117;
+			
+			// aapt resource value: 0x7f050001
+			public const int largeTextBox = 2131034113;
 			
 			static Id()
 			{
@@ -95,11 +110,23 @@ namespace BlindsPort
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7f040005
+			public const int app_name = 2130968581;
+			
+			// aapt resource value: 0x7f040003
+			public const int dummy_button_one = 2130968579;
+			
+			// aapt resource value: 0x7f040004
+			public const int dummy_button_two = 2130968580;
 			
 			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			public const int dummy_text_large = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int dummy_text_medium = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int dummy_text_small = 2130968578;
 			
 			static String()
 			{
